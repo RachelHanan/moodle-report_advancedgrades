fd1f38b4739c9890c309217e0261930a0140eef5
marcusgreen <marcusavgreen@gmail.com>
2016-04-09 06:56:48 +0100

    replaced literals with get_string

--- rubric.php
+++ rubric.php
@@ -99,10 +99,11 @@ foreach ($data as $line) {
     }
     $sheet->write_string(4, $pos, $line->description, $format);
     $sheet->merge_cells(4, $pos, 4, $pos + 2, $format);
-    $sheet->write_string(5, $pos, 'Score', $format2);
+    $sheet->write_string(5, $pos, get_string('score','report_componentgrades'), $format2);
     $sheet->set_column($pos, $pos++, 6); // Set column width to 6.
-    $sheet->write_string(5, $pos++, 'Definition', $format2);
-    $sheet->write_string(5, $pos, 'Feedback', $format2);
+    $sheet->write_string(5, $pos++, get_string('definition','report_componentgrades'), $format2);
+    $sheet->write_string(5, $pos, get_string('feedback','report_componentgrades'), $format2);
+   
     $sheet->set_column($pos - 1, $pos++, 10); // Set column widths to 10.
 }
 
